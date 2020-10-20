7756c4f3fc8c9eb35f552337e8e7a8d8c2021fbc
thiles03 <thiles03@gmail.com>
2020-10-20 13:43:51 +0100

    edit

---
+++ Source/ThirdPersonSurvival/CPP_TPS_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CPP_TPS_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONSURVIVAL_API UCPP_TPS_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ThirdPersonSurvival/CPP_TPS_PlayerCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_TPS_PlayerCharacter.h"
+
+// Sets default values
+ACPP_TPS_PlayerCharacter::ACPP_TPS_PlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_TPS_PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_TPS_PlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACPP_TPS_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
