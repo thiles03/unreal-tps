cff94ff107da35d38d8d1547cde68bc330aff330
thiles03 <thiles03@gmail.com>
2020-12-02 18:10:53 +0000

    BP to CPP

--- Source/ThirdPersonSurvival/CPP_TPS_PlayerCharacter.cpp
+++ Source/ThirdPersonSurvival/CPP_TPS_PlayerCharacter.cpp
@@ -1,31 +1,33 @@
 #include "CPP_TPS_PlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ACPP_TPS_PlayerCharacter::ACPP_TPS_PlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
 void ACPP_TPS_PlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ACPP_TPS_PlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
-void ACPP_TPS_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ACPP_TPS_PlayerCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
--- Source/ThirdPersonSurvival/CPP_TPS_PlayerCharacter.h
+++ Source/ThirdPersonSurvival/CPP_TPS_PlayerCharacter.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Character.h"
 #include "CPP_TPS_PlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class THIRDPERSONSURVIVAL_API ACPP_TPS_PlayerCharacter : public ACharacter
 {
@@ -12,16 +15,18 @@ class THIRDPERSONSURVIVAL_API ACPP_TPS_PlayerCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ACPP_TPS_PlayerCharacter();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+private:
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent *SpringArm = nullptr;
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent *Camera = nullptr;
 };
